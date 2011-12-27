e73d94736c5b694111fb74a6c8ae2f95008c51cc
Sumanth Peddamatham <peddamat@gmail.com>
2011-12-27 22:51:02 +0530

    Updated .hgignore so to bring in base/tooling and base/db.

--- lib/import.rb
+++ lib/import.rb
@@ -16,25 +16,19 @@ module SwissLib
       # Extract zip file to a temporary directory
       `unzip -u #{zip_file} -d #{File.join(@tmp_path, @filebase)}/`
 
-      # Cleanup
-      # File.delete(zip_file)
-
       siteurl = hook_import_project update_wpconfig
 
       deploy_project @project_name
       commit_updates siteurl
     end
 
-    # TODO: Clean this up, this code is replicated in project_initializer_staging.rb
     def deploy_project(project_name)
       # Copy project from repository to web server directory
       FileUtils.rm_rf "#{@web_path}/#{project_name}"
       FileUtils.cp_r(File.join(@project_path, 'src', '.'), "#{@web_path}/#{project_name}")
     end
 
-    # TODO: Clean this up, this code is replicated in project_initializer_initialize.rb
     def commit_updates(siteurl)
-      # puts `cd #{@project_path} && hg commit -m "Imported site from #{@siteurl}" -u "www-data"`
       puts `cd #{@project_path} && hg add && hg commit -m "Imported site from #{siteurl}" -u "www-data"`
     end
 
